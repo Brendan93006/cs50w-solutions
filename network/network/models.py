8be88d73b657b2121f3a38f571cd1459f57d4be8
liablepencil49 <bregas930@gmail.com>
2026-01-04 18:11:04 -0600

    Migrated initial models and made Post model for Network project.

--- network/network/models.py
+++ network/network/models.py
@@ -4,3 +4,7 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField()
