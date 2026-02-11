a9847424e7cf638c09e9a0fbe976b7cb34190756
liablepencil49 <bregas930@gmail.com>
2026-02-10 19:42:30 -0600

    Added logic to shifts page and add_shift, I also added design to my pages.

--- final_project/LaborIQ/schedulr/urls.py
+++ final_project/LaborIQ/schedulr/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('add_employee', views.add_employee, name='add_employee'),
-    path('employees', views.employees_view, name='employees')
+    path('employees', views.employees_view, name='employees'),
+    path('shifts', views.shifts_view, name='shifts'),
+    path('add_shift', views.add_shift, name='add_shift')
 ]
--- final_project/LaborIQ/schedulr/views.py
+++ final_project/LaborIQ/schedulr/views.py
@@ -4,12 +4,16 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadReque
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 
 from .models import User, Employee, Shift
+
+from datetime import date, timedelta, time, datetime
 
 # Create your views here.
 
 def index(request):
+
     return render(request, 'index.html')
 
 def register(request):
@@ -34,7 +38,7 @@ def register(request):
         return render(request, 'register.html')
     
 def login_view(request):
-    if request == 'POST':
+    if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
 
@@ -65,17 +69,55 @@ def add_employee(request):
         else:
             employee = Employee.objects.create(name=name, position=position, hourly_rate=hourly_rate, owner=request.user)
             employee.save()
-        
-        return render(request, 'employees.html')
+            return HttpResponseRedirect(reverse('employees'))
     
     else:
         return render(request, 'add_employee.html')
 
 @login_required
 def employees_view(request):
+    if request.method == 'GET': 
+        employees = Employee.objects.filter(owner=request.user).all()
+        if employees.count() == 0:
+            return render(request, 'employees.html', { "employees": employees, "message": "No employees available" })
+        else:
+            return render(request, 'employees.html', { "employees": employees })
+        
+@login_required
+def shifts_view(request):
     if request.method == 'GET':
-        try: 
-            employees = Employee.objects.get(owner=request.user).all()
-        except OperationalError:
-            return render(request, 'employees.html', { "message": "No employees available" })
-        return render(request, 'employees.html', { "employees": employees })
+        today = timezone.localdate()
+
+        start_of_week = today - timedelta(days=today.weekday())
+        week_start = datetime.combine(start_of_week, time.min)
+        week_end = week_start + timedelta(days=7)
+
+        tz = timezone.get_current_timezone()
+        week_start = timezone.make_aware(week_start, timezone=tz)
+        week_end = timezone.make_aware(week_end, timezone=tz)
+
+        shifts = Shift.objects.filter(start_time__lt=week_end, end_time__gte=week_start).order_by("start_time")
+
+        return render(request, 'shifts.html', {"shifts": shifts})
+    elif request.method == 'POST':
+        return render(request, 'add_shift.html')
+
+    
+@login_required
+def add_shift(request):
+    if request.method == 'POST':
+        employee = get_object_or_404(Employee, id=request.POST.get("employee_id"))
+
+        start_time = request.POST.get("start_time")
+
+        end_time = request.POST.get("end_time")
+
+        if not employee or not start_time or not end_time:
+            return render(request, 'add_shift.html', { "message": "Must provide employee/start time/end time." })
+        else:
+            shift = Shift.objects.create(employee=employee, start_time=start_time, end_time=end_time)
+            shift.save()
+            return HttpResponseRedirect(reverse('shifts'))
+    elif request.method == 'GET':
+        employees = Employee.objects.filter(owner=request.user).all()
+        return render(request, 'add_shift.html', { "employees": employees })
